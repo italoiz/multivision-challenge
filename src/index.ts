01819a2146eafd069c2aba96b558918182fc128a
Italo Izaac <italoiz.dev@gmail.com>
2023-01-17 17:59:26 -0300

    chore: Add refresh button to project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import { Letter } from "./letter";
 
-window.onload = function () {
-  new Letter().get().then((content) => {
-    document.body.innerHTML = `<pre>${JSON.stringify(content, null, 2)}</pre>`;
-  });
-};
+const refreshButton = document.getElementById('refresh-btn');
+const contentElement = document.getElementById('result');
+const letter = new Letter();
+
+function preBlock(content: string): string {
+  return `<pre>${content}</pre>`;
+}
+
+function getLetter() {
+  return letter.get().then(content => JSON.stringify(content, null, 2));
+}
+
+async function fillContentEl() {
+  if (!!contentElement) {
+    contentElement.innerHTML = preBlock(await getLetter());
+  }
+}
+
+window.onload = fillContentEl;
+
+refreshButton?.addEventListener('click', fillContentEl);
